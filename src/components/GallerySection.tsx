7e39efc3dc3dd860d30aa41ee58bca8a3d36dde8
Nicky753951 <79370880+Nicky753951@users.noreply.github.com>
2026-03-28 19:02:29 +0200

    Update GallerySection.tsx

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -10,15 +10,11 @@ import {
 import gallery1 from "@/assets/gallery-1.jpg";
 import gallery2 from "@/assets/gallery-2.jpg";
 import gallery3 from "@/assets/gallery-3.jpg";
-import gallery4 from "@/assets/gallery-4.jpg";
-import gallery5 from "@/assets/gallery-5.jpg";
 
 const photos = [
   { src: gallery1, alt: "Наше фото 1" },
   { src: gallery2, alt: "Наше фото 2" },
   { src: gallery3, alt: "Наше фото 3" },
-  { src: gallery4, alt: "Наше фото 4" },
-  { src: gallery5, alt: "Наше фото 5" },
 ];
 
 const GallerySection = () => {
