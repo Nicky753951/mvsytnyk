5160590f8f63b642667db563fd922282ed0a502e
Nicky753951 <79370880+Nicky753951@users.noreply.github.com>
2026-03-29 13:01:52 +0300

    Update BlogSection.tsx

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -32,7 +32,7 @@ const BlogSection = () => {
         transition={{ duration: 0.8 }}
       >
         <p className="wedding-subheading text-muted-foreground mb-2">Моменти щастя</p>
-        <h2 className="wedding-heading text-foreground mb-4">Наш блог</h2>
+        <h2 className="wedding-heading text-foreground mb-4">Наш фотоблог</h2>
         <div className="gold-divider" />
       </motion.div>
 
