c02cc047ba1d41eb3f71ad60517dd05e08208e6c
Nicky753951 <79370880+Nicky753951@users.noreply.github.com>
2026-03-29 19:16:46 +0300

    Update BlogSection.tsx

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -12,6 +12,9 @@ import gallery2 from "@/assets/gallery-2.jpg";
 import gallery3 from "@/assets/gallery-3.jpg";
 import gallery4 from "@/assets/gallery-4.jpg";
 import gallery5 from "@/assets/gallery-5.jpg";
+import gallery6 from "@/assets/gallery-6.jpg";
+import gallery7 from "@/assets/gallery-7.jpg";
+import gallery8 from "@/assets/gallery-8.jpg";
 
 const posts = [
 {src: gallery3, date: "15 березня 2026", caption: "А ми вже обрали весільні обручки ♾️" },
