c47889fedfe756273ba9c2e94d9207030320f8ac
Nicky753951 <79370880+Nicky753951@users.noreply.github.com>
2026-03-30 00:01:07 +0300

    Update DetailsSection.tsx

--- src/components/DetailsSection.tsx
+++ src/components/DetailsSection.tsx
@@ -5,7 +5,6 @@ const details = [
   {
     icon: PartyPopper,
     title: "Локація",
-    time: "14:00",
     place: "Ресторан «Софіївський посад»",
     link: "https://www.google.com/maps?q=%D0%A1%D0%BE%D1%84%D1%96%D1%97%D0%B2%D1%81%D1%8C%D0%BA%D0%B8%D0%B9+%D0%9F%D0%BE%D1%81%D0%B0%D0%B4,+%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%9A%D0%B8%D1%97%D0%B2%D1%81%D1%8C%D0%BA%D0%B0,+81/2,+%D0%A1%D0%BE%D1%84%D1%96%D1%97%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%91%D0%BE%D1%80%D1%89%D0%B0%D0%B3%D1%96%D0%B2%D0%BA%D0%B0,+%D0%9A%D0%B8%D1%97%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+08131&ftid=0x40d4cbc6c11f7db9:0x87205c1af3bbb4d&entry=gps&shh=CAE&lucs=,94297699,94284493,94231188,94280568,47071704,94218641,94282134,100799872,94286869&g_ep=CAISEjI2LjEyLjIuODg0NjExMjE2MBgAIIgnKlIsOTQyOTc2OTksOTQyODQ0OTMsOTQyMzExODgsOTQyODA1NjgsNDcwNzE3MDQsOTQyMTg2NDEsOTQyODIxMzQsMTAwNzk5ODcyLDk0Mjg2ODY5QgJVQQ%3D%3D&skid=56b301d8-9a91-47a2-a09a-389e4eeca823&g_st=it&g_st=it",
     address: "Київська область, с. Софіївська Борщагівка, вул. Київська, 81/2",
